87534ff4da95050c93ae6451669e87a8ea4fe8d0
Joshua Grossberg <joshgrossberg@gmail.com>
2018-01-29 09:55:17 -0800

    increase timeout

--- src/api/index.js
+++ src/api/index.js
@@ -12,7 +12,7 @@ console.log('JG: BASE_URL = ', BASE_URL);
 const Api = axios.create({
     baseURL: BASE_URL,
     headers,
-    timeout: 6000,
+    timeout: 10000,
     transformRequest: [ data => { if (data) return qs.stringify(data) } ]
 });
 
