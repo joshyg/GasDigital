ba691f5ce96e36ccb836fd4d055d50923a89b8d0
Joshua Grossberg <joshgrossberg@gmail.com>
2018-03-14 21:19:32 -0700

    prettier update

--- src/constants.js
+++ src/constants.js
@@ -1,57 +1,57 @@
 export const colors = {
-  yellow: "#fcf411",
-  blue: "#0d54ba",
-  darkBlue: "#246773",
-  facebookBlue: "#3B5998",
-  red: "#C23535",
-  white: "#ffffff",
-  white3: "#d8d8d8",
-  black: "#000",
-  dark: "#252525",
-  grey1: "#E6E7E8",
-  grey2: "#717171",
-  grey3: "#484848",
-  grey4: "#3A3A3A",
-  grey5: "#373737",
-  buttonGrey: "#8e8e93",
-  transparent: "rgba(0,0,0,0)",
-  bodyBackground: "#191919",
-  headerBackground: "rgba(37, 37, 37, 0.5)",
-  footerBakground: "#2e2e2e"
+  yellow: '#fcf411',
+  blue: '#0d54ba',
+  darkBlue: '#246773',
+  facebookBlue: '#3B5998',
+  red: '#C23535',
+  white: '#ffffff',
+  white3: '#d8d8d8',
+  black: '#000',
+  dark: '#252525',
+  grey1: '#E6E7E8',
+  grey2: '#717171',
+  grey3: '#484848',
+  grey4: '#3A3A3A',
+  grey5: '#373737',
+  buttonGrey: '#8e8e93',
+  transparent: 'rgba(0,0,0,0)',
+  bodyBackground: '#191919',
+  headerBackground: 'rgba(37, 37, 37, 0.5)',
+  footerBakground: '#2e2e2e',
 };
 
 export const fonts = {
-  light: "Lato-Light",
-  regular: "Lato-Regular",
-  bold: "Lato-Bold",
-  black: "Lato-Black"
+  light: 'Lato-Light',
+  regular: 'Lato-Regular',
+  bold: 'Lato-Bold',
+  black: 'Lato-Black',
 };
 
 // enum
 export const offlineDownloadStatus = {
-  notDownloaded: "notDownloaded",
-  downloading: "downloading",
-  downloaded: "downloaded"
+  notDownloaded: 'notDownloaded',
+  downloading: 'downloading',
+  downloaded: 'downloaded',
 };
 
 export const iconNames = {
-  homescreen: "headphones",
-  live: "video-camera",
-  search: "search",
-  library: "download",
-  settings: "cog"
+  homescreen: 'headphones',
+  live: 'video-camera',
+  search: 'search',
+  library: 'download',
+  settings: 'cog',
 };
 
 export const routeHeaders = {
-  homescreen: "Listen",
-  live: "Live",
-  search: "Search",
-  library: "Library",
-  settings: "Settings"
+  homescreen: 'Listen',
+  live: 'Live',
+  search: 'Search',
+  library: 'Library',
+  settings: 'Settings',
 };
 
 // export const BASE_URL = 'http://rss.gasdigitalnetwork.com/';
-export const BASE_URL = "https://gasdigitalnetwork.com/";
+export const BASE_URL = 'https://gasdigitalnetwork.com/';
 export const ENABLE_DOWNLOAD_VIDEO = false;
 export const DEBUG_PREMIUM = false;
 export const DEBUG_CRASH = false;
