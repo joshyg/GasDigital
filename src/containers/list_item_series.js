2e85c357536567645d17998399b295e7eb376ec3
Joshua Grossberg <joshgrossberg@gmail.com>
2018-03-17 21:39:10 -0700

    android home style fix

--- src/containers/list_item_series.js
+++ src/containers/list_item_series.js
@@ -40,11 +40,11 @@ const iconWidth = width >= 400 ? 170 : width >= 375 ? 150 : 130;
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    height: iconWidth,
+    height: iconWidth + 10,
     width: width / 2,
-    paddingBottom: 10,
+    paddingBottom: 5,
     marginTop: width > 400 ? 15 : 10,
-    marginBottom: 10,
+    marginBottom: 5,
     alignItems: 'center',
     backgroundColor: colors.bodyBackground,
   },
